2ecdd315f765884331f3595274306e40d20dc872
scorsi <sylvain.corsini@protonmail.com>
2020-06-26 01:11:18 +0200

    Remove GOOS and GOARCH from go_binary rule and put them in build cmd in cli

--- prometer-hello-world/build_and_deploy.sh
+++ prometer-hello-world/build_and_deploy.sh
@@ -1,1 +1,2 @@
-bazel build //prometer-hello-world:pkg && terraform apply -auto-approve
+bazel build --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 //prometer-hello-world:bin &&
+  terraform apply -auto-approve
